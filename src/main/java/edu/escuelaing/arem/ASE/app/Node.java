b550edbbb741d802f16cb8d531af02b0d47f1c2b
Santiago <srubianof@icloud.com>
2020-08-06 17:51:15 -0500

    Creando nodo

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/Node.java
@@ -0,0 +1,30 @@
+package edu.escuelaing.arem.ASE.app;
+
+/**
+ * @author Santiago
+ */
+public class Node {
+    private double data;
+    private Node next;
+
+    public double getData() {
+        return data;
+    }
+
+    public void setData(double data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(double data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
