13f017eb973dd6103f11f8c4a94fe2405047bad2
Santiago <srubianof@icloud.com>
2020-08-07 00:23:30 -0500

    Mean and SD calculator, correction of LinkedLists

--- src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
@@ -4,15 +4,43 @@ public class LinkedList {
     private Node head;
     private Node tail;
 
-    public void insertarNodo( double data){
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public static void printLinkedList(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            System.out.format(node.getData() + " ");
+            printLinkedList(node.getNext());
+        }
+    }
+
+    public void insertNode(double data) {
         Node nodo = new Node();
         nodo.setData(data);
         nodo.setNext(null);
-        if (head == null){
+        if (head == null) {
             head = nodo;
+            tail = nodo;
         } else {
             tail.setNext(nodo);
             tail = nodo;
         }
     }
+
+
 }
